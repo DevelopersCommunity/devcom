926158fdedfbc48e00d3ac52851bc380409f9647
Felipe Fujiy Pessoto <felipepessoto@hotmail.com>
2016-02-14 00:22:23 -0200

    Exemplo de performance

---
+++ ExceptionPerformance/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionPerformance
+{
+    class Program
+    {
+        static Func<string, bool> MethodToBenchmark = ValidateNumberWithException;
+
+        static void Main(string[] args)
+        {
+            const int iterations = 100000;
+            const string number = "123devcom";
+
+            MethodToBenchmark(number);
+
+            Stopwatch c = Stopwatch.StartNew();
+
+            for (int i = 0; i < iterations; i++)
+            {
+                MethodToBenchmark(number);
+            }
+
+            c.Stop();
+            Console.WriteLine(c.ElapsedMilliseconds + "ms");
+
+            //Console.ReadLine();
+        }
+
+        private static bool ValidateNumberWithException(string number)
+        {
+            try
+            {
+                int.Parse(number);
+            }
+            catch (FormatException)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private static bool ValidateNumberWithoutException(string number)
+        {
+            int value;
+            return int.TryParse(number, out value);
+        }
+    }
+}
